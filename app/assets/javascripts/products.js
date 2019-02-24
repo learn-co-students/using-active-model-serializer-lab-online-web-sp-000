185658f57dc81ef070e2437acd6bee00a28d9d21
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-24 17:52:33 -0500

    implement "next" on show page, in separate JS file

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,11 +1,22 @@
 function showProduct(id) {
   $.get("/products/" + id + ".json", function (product) {
-    // debugger
     let prefix = `#product-${id}-`
     $(prefix+'description').html(product.description)
     var inventoryText = "<b>Available</b>"
     if (product.inventory < 0)
       inventoryText = "<b>Sold Out</b>"     
     $(prefix + 'inventory').html(inventoryText)
+  });
+}
+
+function loadNext(currentId) {
+  var nextId = currentId + 1;
+  $.get("/products/" + nextId + ".json", function (data) {
+    $(".productName").text(data["name"]);
+    $(".productPrice").text(data["price"]);
+    $(".productDescription").text(data["description"]);
+    $(".productInventory").text(data["inventory"]);
+    // re-set the id to current on the link
+    $(".js-next").attr("data-id", data["id"]);
   });
 }
