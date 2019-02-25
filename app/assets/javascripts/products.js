0d9de800e90a26cafe2d6acaaa00fa54867bcc03
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-25 09:07:50 -0500

    change vars to lets in JS

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,7 +2,7 @@ function showProduct(id) {
   $.get("/products/" + id + ".json", function (product) {
     let prefix = `#product-${id}-`
     $(prefix+'description').html(product.description)
-    var inventoryText = "<b>Available</b>"
+    let inventoryText = "<b>Available</b>"
     if (product.inventory < 0)
       inventoryText = "<b>Sold Out</b>"     
     $(prefix + 'inventory').html(inventoryText)
@@ -10,7 +10,7 @@ function showProduct(id) {
 }
 
 function loadNext(currentId) {
-  var nextId = currentId + 1;
+  let nextId = currentId + 1;
   $.get("/products/" + nextId + ".json", function (data) {
     $(".productName").text(data["name"]);
     $(".productPrice").text(data["price"]);
