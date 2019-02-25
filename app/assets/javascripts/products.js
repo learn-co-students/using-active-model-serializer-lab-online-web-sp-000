e20d9f348d3b8656c5ff02ac8e4410b9c9c9c7b0
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-25 09:33:09 -0500

    wrap all product info in div

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,18 +1,22 @@
 function showProduct(id) {
-  $.get("/products/" + id + ".json", function (product) {
-    let prefix = `#product-${id}-`
-    $(prefix+'description').html(product.description)
-    let inventoryText = "<b>Available</b>"
-    if (product.inventory < 0)
-      inventoryText = "<b>Sold Out</b>"     
-    $(prefix + 'inventory').html(inventoryText)
+  $.get("/products/" + id + ".json", function(product) {
+    let div = `#product-${id} `;
+    $(div + ".description").html(product.description);
+    let inventoryText = "<b>Available</b>";
+    if (product.inventory < 0) inventoryText = "<b>Sold Out</b>";
+    $(div + ".inventory").html(inventoryText);
+    showOrders(product);
   });
-  let orders = product.orders
+}
+
+function showOrders(product) {
+  for (order of product.orders) {
+  }
 }
 
 function loadNext(currentId) {
   let nextId = currentId + 1;
-  $.get("/products/" + nextId + ".json", function (data) {
+  $.get("/products/" + nextId + ".json", function(data) {
     $(".productName").text(data["name"]);
     $(".productPrice").text(data["price"]);
     $(".productDescription").text(data["description"]);
